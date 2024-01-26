73e3eb64b01b5abfc9167fe36344cf2c99e7f86f
Rohan-Beta <rohitraha952@gmail.com>
2024-01-26 21:07:25 +0530

    add project

---
+++ android/app/src/main/kotlin/rohan/app/amazon/MainActivity.kt
@@ -0,0 +1,6 @@
+package rohan.app.amazon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCPKT7FtUBTszbepHhm046s8SFXG2UJmyk",
+          authDomain: "clone-a1a2e.firebaseapp.com",
+          projectId: "clone-a1a2e",
+          storageBucket: "clone-a1a2e.appspot.com",
+          messagingSenderId: "644371332922",
+          appId: "1:644371332922:web:49e99d3ca1a120cc62da03",
+          measurementId: "G-1KP9F0MC7C"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const AmazonClone());
+}
+
+class AmazonClone extends StatelessWidget {
+  const AmazonClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Amazon Clone",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(child: Text("Hello World")),
+        ),
+      ),
+    );
+  }
+}
